32f623af3f21003d75280f186537fe74a6755213
Jasse Lahdenperä <jazkuli@msn.com>
2011-12-21 19:17:36 +0200

    Added code for testing and fixed some errors

---
+++ tools/build_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get Script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CB="$DIR/cbcompiler/CB.exe"
+
+cd "$DIR/../tests"
+
+# Process all .cb files
+for i in `ls | grep \.cb`
+do
+	echo "Building $i"
+	name=`echo $i|cut -d'.' -f1`
+	wine $CB $i
+	
+	mv "$DIR/cbcompiler/$name.exe" exe/
+done
+
+
