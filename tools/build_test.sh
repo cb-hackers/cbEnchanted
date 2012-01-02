3ae1e01af11a9cb953ce18d506341314e6a9fe31
Tapio Vierros <tapiovierros@gmail.com>
2012-01-02 21:16:25 +0200

    Build_test.sh cretes exe dir if it doesn't exist.

--- tools/build_test.sh
+++ tools/build_test.sh
@@ -3,8 +3,11 @@
 # Get Script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CB="$DIR/cbcompiler/CB.exe"
+OUTDIR="$DIR/../tests/exe"
 
+mkdir -p "$OUTDIR"
 cd "$DIR/../tests"
+
 
 # Process all .cb files
 for i in `ls | grep \.cb`
@@ -13,7 +16,7 @@ do
 	name=`echo $i|cut -d'.' -f1`
 	wine $CB $i
 	
-	mv "$DIR/cbcompiler/$name.exe" exe/
+	mv "$DIR/cbcompiler/$name.exe" "$OUTDIR"
 done
 
 
