a0fd8e5f7bd5cc09a6062966c04cd579712efeb8
Lassi Hämäläinen <Latexi95@gmail.com>
2012-02-11 18:19:03 +0200

    Little cleaning and some updates to CBMap.

--- src/cbmap.h
+++ src/cbmap.h
@@ -7,44 +7,41 @@
 class CBMap : public CBObject{
 
 
-                int32_t *layer[4];
+		int32_t *layer[4];
 
-                int32_t tilecount;
-                int32_t mapWidth;
-                int32_t mapHeight;
-                int32_t tileWidth;
-                int32_t tileHeight;
+		int32_t tilecount;
+		int32_t mapWidth;
+		int32_t mapHeight;
+		int32_t tileWidth;
+		int32_t tileHeight;
 
-		sf::Image *tilesetImage;
-		sf::Texture *tilesetTexture;
-		sf::Sprite tilesetSprite;
 
 		uint8_t maskR, maskG, maskB;
 
-                int32_t *animLenght;
-                int32_t *animSlowness;
+		int32_t *animLenght;
+		int32_t *animSlowness;
 
-        public:
-                CBMap();
+	public:
+		CBMap();
 
-                ~CBMap();
+		~CBMap();
 
 
-                bool loadMap(string path);
-                bool loadTileset(string path);
+		bool loadMap(string path);
+		bool loadTileset(string path);
 
-                void drawBackLayer(sf::RenderTarget &target, float camX, float camY);
-                void drawOverLayer(sf::RenderTarget &target, float camX, float camY);
+		void drawBackLayer(RenderTarget &target, float camX, float camY);
+		void drawOverLayer(RenderTarget &target, float camX, float camY);
+		void drawTile(int32_t tile,float x,float y);
 
-                void edit(uint8_t maplayer, int32_t MapX, int32_t MapY, int32_t tile);
-                void maskObject(uint8_t r, uint8_t g, uint8_t b);
+		void edit(uint8_t maplayer, int32_t MapX, int32_t MapY, int32_t tile);
 
-                int32_t mapSizeX(){return mapWidth;}
-                int32_t mapSizeY(){return mapHeight;}
+		int32_t mapSizeX(){return mapWidth;}
+		int32_t mapSizeY(){return mapHeight;}
 
-                int32_t getSizeX(){return mapWidth*tileWidth;}
-                int32_t getSizeY(){return mapHeight*tileHeight;}
-                void playObject();
+		int32_t getSizeX(){return mapWidth*tileWidth;}
+		int32_t getSizeY(){return mapHeight*tileHeight;}
+		void playObject();
 };
 
 
