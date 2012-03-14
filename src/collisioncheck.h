3b7140659d62fc36be893a7bb70ac4df39892aad
Marko Ranta <marko.ranta95@luukku.com>
2012-03-14 22:08:08 +0200

    collisioncheckkiin vähän täytettä.

--- src/collisioncheck.h
+++ src/collisioncheck.h
@@ -15,14 +15,18 @@ public:
 
 	CollisionCheck();
 	bool isNull()const{return mObject1 == 0 || mObject2 == 0;}
-    void setObjects(CBObject *a,CBObject *b){mObject1 = a;mObject2 = b;}
+	void setObjects(CBObject *a,CBObject *b){mObject1 = a;mObject2 = b;}
 	void setCollisionType1(CollisionType c){mCollisionType1 = c;}
 	void setCollisionType2(CollisionType c){mCollisionType2 = c;}
+	bool testCollision();
+	void handleCollision();
 private:
 	CBObject *mObject1;
 	CBObject *mObject2;
 	CollisionType mCollisionType1,mCollisionType2;
-
+	float mCollisionX, mCollisionY;
+	float mCollisionAngle;
+	uint16_t collisionCount;
 };
 
 #endif // COLLISIONCHECK_H
