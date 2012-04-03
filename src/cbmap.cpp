19470c2be256dfa98b15a13eb335216b557cf24b
Vesa Laakso <laakso.vesa@gmail.com>
2012-04-03 11:46:15 +0300

    Cleaned and documented CBMap

--- src/cbmap.cpp
+++ src/cbmap.cpp
@@ -2,53 +2,61 @@
 #include "cbenchanted.h"
 #include "precomp.h"
 
-CBMap::CBMap():CBObject(){
+/** Constructs a new CBMap and calls the constructor for CBObject */
+CBMap::CBMap() : CBObject() {
 	layerShowing[0] = 1;
 	layerShowing[1] = 1;
 }
 
-
-CBMap::~CBMap(){
-	for(int i = 0; i < 4; ++i){
+/** Destroys all layers and frees resources */
+CBMap::~CBMap() {
+	for (int i = 0; i < 4; ++i) {
 		delete [] layers[i];
 	}
-	delete [] animLenght;
+	delete [] animLength;
 	delete [] animSlowness;
 	delete [] currentFrame;
 }
 
-bool CBMap::create(uint32_t width, uint32_t height, uint16_t tileW, uint16_t tileH){
+/** Creates a new map
+ * @param width The width of the map, in tiles
+ * @param height The height of the map, in tiles
+ * @param tileW Width of a single tile, in pixels
+ * @param tileH Height of a single tile, in pixels
+ */
+bool CBMap::create(uint32_t width, uint32_t height, uint16_t tileW, uint16_t tileH) {
 	mapWidth = width;
 	mapHeight = height;
 	tileWidth = tileW;
 	tileHeight = tileH;
-	for(int i = 0; i < 4; ++i){
-		layers[i] = new int32_t[mapWidth*mapHeight];
+	for (int i = 0; i < 4; ++i) {
+		layers[i] = new int32_t[mapWidth * mapHeight];
 	}
 	return true;
 }
 
-bool CBMap::loadMap(string file){
-
-
+/** Loads a map from file.
+ * @param file Path to mapfile
+ */
+bool CBMap::loadMap(string file) {
 	fstream mapStream(file.c_str(), ios::binary | ios::in);
 	unsigned char checkNum[4];
 	float version;
 	uint8_t empty;
 
-	if(mapStream.is_open()){
+	if (mapStream.is_open()) {
 
 		//Tarkistustavut...
 		mapStream.read((char*)checkNum, 4);
 
-		if(checkNum[0] != 40  || checkNum[1] != 192 || checkNum[2] != 13 || checkNum[3] != 139){
-			FIXME("Map: Incorrect magic nums (%u, %u,%u, %u)!",checkNum[0],checkNum[1],checkNum[2],checkNum[3]);
+		if (checkNum[0] != 40 || checkNum[1] != 192 || checkNum[2] != 13 || checkNum[3] != 139) {
+			FIXME("Map: Incorrect magic nums (%u, %u,%u, %u)!", checkNum[0], checkNum[1] ,checkNum[2], checkNum[3]);
 		}
 
 
 		mapStream.read((char*)&version, 4);
-		if(!(version>=1.0 && version <= 2.0)){
-			FIXME("Version isn't right!")
+		if (!(version>=1.0 && version <= 2.0)) {
+			FIXME("Version isn't right!");
 		}
 
 
@@ -68,7 +76,7 @@ bool CBMap::loadMap(string file){
 			return false;
 		}
 
-		animLenght = new int32_t [tileCount];
+		animLength = new int32_t [tileCount];
 		animSlowness = new int32_t [tileCount];
 		currentFrame = new float [tileCount];
 		memset(currentFrame,0,sizeof(float)*tileCount);
@@ -79,177 +87,238 @@ bool CBMap::loadMap(string file){
 		mapStream.read((char*)&mapWidth, 4);
 		mapStream.read((char*)&mapHeight, 4);
 
-		for(int i = 0; i < 4; ++i){
-			layers[i] = new int32_t[mapWidth*mapHeight];
+		for (int i = 0; i < 4; ++i) {
+			layers[i] = new int32_t [mapWidth * mapHeight];
 		}
 
 		mapStream.read((char*)checkNum, 4);
 
 		//Karttadata alkaa
-		if(checkNum[0] != 254  ||
-		   checkNum[1] != 45   ||
-		   checkNum[2] != 12   ||
-		   checkNum[3] != 166)
-			FIXME("Map layer0: Incorrect magic nums (%u, %u,%u, %u)!",checkNum[0],checkNum[1],checkNum[2],checkNum[3]);
+		if (checkNum[0] != 254 ||
+			checkNum[1] != 45 ||
+			checkNum[2] != 12 ||
+			checkNum[3] != 166
+		) {
+			FIXME("Map layer0: Incorrect magic nums (%u, %u,%u, %u)!", checkNum[0], checkNum[1], checkNum[2], checkNum[3]);
+		}
 
-		mapStream.read((char*)layers[0], 4*mapHeight*mapWidth);
+		mapStream.read((char*)layers[0], 4 * mapHeight * mapWidth);
 
 		mapStream.read((char*)checkNum, 4);
 
-		if(checkNum[0] != 253  ||
-		   checkNum[1] != 44   ||
-		   checkNum[2] != 11   ||
-		   checkNum[3] != 165)
-			FIXME("Map layer1: Incorrect magic nums (%u, %u,%u, %u)!",checkNum[0],checkNum[1],checkNum[2],checkNum[3]);
+		if (checkNum[0] != 253 ||
+			checkNum[1] != 44 ||
+			checkNum[2] != 11 ||
+			checkNum[3] != 165
+		) {
+			FIXME("Map layer1: Incorrect magic nums (%u, %u,%u, %u)!", checkNum[0], checkNum[1], checkNum[2], checkNum[3]);
+		}
 
 		//Collision layer
-		mapStream.read((char*)layers[2], 4*mapHeight*mapWidth);
+		mapStream.read((char*)layers[2], 4 * mapHeight * mapWidth);
 
 		mapStream.read((char*)checkNum, 4);
 
-		if(checkNum[0] != 252  ||
-		   checkNum[1] != 43   ||
-		   checkNum[2] != 10   ||
-		   checkNum[3] != 164)
-			FIXME("Map layer2: Incorrect magic nums (%u, %u,%u, %u)!",checkNum[0],checkNum[1],checkNum[2],checkNum[3]);
+		if (checkNum[0] != 252 ||
+			checkNum[1] != 43 ||
+			checkNum[2] != 10 ||
+			checkNum[3] != 164
+		) {
+			FIXME("Map layer2: Incorrect magic nums (%u, %u,%u, %u)!", checkNum[0], checkNum[1], checkNum[2], checkNum[3]);
+		}
 
 		mapStream.read((char*)layers[1], 4*mapHeight*mapWidth);
 
 		mapStream.read((char*)checkNum, 4);
 
-		if(checkNum[0] != 251  &&
-		   checkNum[1] != 42   &&
-		   checkNum[2] != 9    &&
-		   checkNum[3] != 163)
-			FIXME("Map layer3: Incorrect magic nums (%u, %u,%u, %u)!",checkNum[0],checkNum[1],checkNum[2],checkNum[3]);
+		if (checkNum[0] != 251 ||
+			checkNum[1] != 42 ||
+			checkNum[2] != 9 ||
+			checkNum[3] != 163
+		) {
+			FIXME("Map layer3: Incorrect magic nums (%u, %u,%u, %u)!", checkNum[0], checkNum[1], checkNum[2], checkNum[3]);
+		}
 
-		mapStream.read((char*)layers[3], 4*mapHeight*mapWidth);
+		mapStream.read((char*)layers[3], 4 * mapHeight * mapWidth);
 
 		mapStream.read((char*)checkNum, 4);
 
-		if(checkNum[0] != 250  ||
-		   checkNum[1] != 41   ||
-		   checkNum[2] != 8    ||
-		   checkNum[3] != 162)
-			FIXME("Tiles: Incorrect magic nums (%u, %u,%u, %u)!",checkNum[0],checkNum[1],checkNum[2],checkNum[3]);
+		if (checkNum[0] != 250 ||
+			checkNum[1] != 41 ||
+			checkNum[2] != 8 ||
+			checkNum[3] != 162
+		) {
+			FIXME("Tiles: Incorrect magic nums (%u, %u,%u, %u)!", checkNum[0], checkNum[1], checkNum[2], checkNum[3]);
+		}
 
-		for(int32_t i = 1; i < tileCount; ++i){
-			mapStream.read((char*)&animLenght[i], 4);
+		for (int32_t i = 1; i < tileCount; ++i) {
+			mapStream.read((char*)&animLength[i], 4);
 			mapStream.read((char*)&animSlowness[i], 4);
 		}
 
-	}else{
-		string err = "Can not open: "+file;
+	}
+	else { // mapstream.is_open() returned false
+		string err = "Can not open: " + file;
 		INFO(err.c_str());
 	}
 	mapStream.close();
-	sizeX = tileWidth*mapWidth;
-	sizeY = tileHeight*mapHeight;
+	sizeX = tileWidth * mapWidth;
+	sizeY = tileHeight * mapHeight;
 	return true;
 }
 
-
-
-bool CBMap::loadTileset(string path){
-	bool success =  load(path,al_map_rgb(maskR,maskG,maskB));
+/** Loads a tileset from an image file.
+ * @param path Path to an image file containing the tilemap
+ */
+bool CBMap::loadTileset(string path) {
+	bool success = load(path, al_map_rgb(maskR, maskG, maskB));
 	return success;
 }
 
-void CBMap::setLayers(uint8_t back, uint8_t over){
+/** Sets layer visibility.
+ * @param back Is the back layer visible or not
+ * @param over Is the over layer visible or not
+ */
+void CBMap::setLayers(uint8_t back, uint8_t over) {
 	layerShowing[0] = back;
 	layerShowing[1] = over;
 }
 
-void CBMap::drawLayer(uint8_t level, RenderTarget &target){
-	if(level > 1)
+/** Draws the given layer to the given RenderTarget.
+ * @param level Which layer should we draw.
+ * @param target To where should we draw, @see RenderTarget
+ */
+void CBMap::drawLayer(uint8_t level, RenderTarget &target) {
+	if (level > 1) {
 		return;
-	if(layerShowing[level] < 1)
+	}
+	if (layerShowing[level] < 1) {
 		return;
-
-	float camX = CBEnchanted::instance()->getCameraX()-posX;
-	float camY = CBEnchanted::instance()->getCameraY()-posY;
-
-
-	int32_t piirto_x = camX+getSizeX()/2-target.width()/2;
-	int32_t piirto_y = -camY+getSizeY()/2-target.height()/2;
-	int32_t tile_y = piirto_y / tileHeight;
-	int32_t jarjestys_y=-(piirto_y % tileHeight);
-
-	while(jarjestys_y < target.height()){
-		tile_y %= getSizeY();
-		if (tile_y >= mapHeight) break;
-		int32_t tile_x = piirto_x / tileWidth;
-		int32_t jarjestys_x =-(piirto_x % tileHeight);
-		while(jarjestys_x < target.width()){
-			int32_t getX = tile_x % getSizeX();
-			if (getX >= mapWidth) break;
-			int32_t tileNum = getMap(level, getX, tile_y);
-			if(tileNum > 0){
-				drawTile(target, tileNum+(int)currentFrame[tileNum], jarjestys_x, jarjestys_y);
-			}
-			tile_x++;
-			jarjestys_x+=tileWidth;
-		}
-		tile_y++;
-		jarjestys_y+=tileHeight;
 	}
 
+	float camX = CBEnchanted::instance()->getCameraX() - posX;
+	float camY = CBEnchanted::instance()->getCameraY() - posY;
 
+
+	int32_t piirto_x = camX + getSizeX() / 2 - target.width() / 2;
+	int32_t piirto_y = -camY + getSizeY() / 2 - target.height() / 2;
+	int32_t tile_y = piirto_y / tileHeight;
+	int32_t jarjestys_y = -(piirto_y % tileHeight);
+
+	while (jarjestys_y < target.height()) {
+		tile_y %= getSizeY();
+		if (tile_y >= mapHeight) {
+			break;
+		}
+
+		int32_t tile_x = piirto_x / tileWidth;
+		int32_t jarjestys_x = -(piirto_x % tileHeight);
+
+		while (jarjestys_x < target.width()) {
+			int32_t getX = tile_x % getSizeX();
+			if (getX >= mapWidth) {
+				break;
+			}
+
+			int32_t tileNum = getMap(level, getX, tile_y);
+			if (tileNum > 0) {
+				drawTile(target, tileNum+(int)currentFrame[tileNum], jarjestys_x, jarjestys_y);
+			}
+
+			tile_x++;
+			jarjestys_x += tileWidth;
+		}
+		tile_y++;
+		jarjestys_y += tileHeight;
+	}
 }
 
-
+/** Draws a single tile.
+ * @param target To where should we draw to, @see RenderTarget
+ * @param tile Which tile should we draw from the tileset, a number from 1 to tile count
+ * @param x,y Drawing coordinates
+ */
 void CBMap::drawTile(RenderTarget &target, int32_t tile, float x, float y) {
-	if(tile == 0)
+	if (tile == 0) {
 		return;
+	}
 	tile--;
 
 	int32_t fX = 0;
 	int32_t fY = 0;
 	int32_t framesX = renderTarget->width() / tileWidth;
 	int32_t framesY = renderTarget->height() / tileHeight;
-	fX = (tile % framesX);
-	fY = (tile / framesY);
+	fX = tile % framesX;
+	fY = tile / framesY;
 
-	target.drawBitmapRegion(texture,fX*tileWidth, fY*tileHeight, tileWidth, tileHeight,x,y);
-
+	target.drawBitmapRegion(texture, fX * tileWidth, fY * tileHeight, tileWidth, tileHeight, x, y);
 }
 
-
+/** Updates the map animation.
+ * @param timestep Does something related to time and step
+ * @returns Always false
+ */
 bool CBMap::updateObject(float timestep) {
-	if (playing) {
-		for(uint16_t tiles = 0; tiles < tileCount; tiles++){
-			if(animLenght[tiles]){
-				currentFrame[tiles] = currentFrame[tiles] + timestep /((float)animSlowness[tiles]*animSpeed);
-				if((int32_t)currentFrame[tiles] > animLenght[tiles]) {
-					currentFrame[tiles] = 0;
-				}
+	if (!playing) {
+		return false;
+	}
+	// Loop through every tile and update the currentFrame array if the tile has animation
+	for (uint16_t tiles = 0; tiles < tileCount; tiles++) {
+		if (animLength[tiles]) {
+			currentFrame[tiles] = currentFrame[tiles] + timestep / ((float)animSlowness[tiles] * animSpeed);
+			if ((int32_t)currentFrame[tiles] > animLength[tiles]) {
+				currentFrame[tiles] = 0;
 			}
 		}
 	}
 	return false;
 }
 
-
-void CBMap::setTile(uint32_t tile, uint32_t lenght, uint32_t slowness){
-	animLenght[tile] = lenght;
+/** Sets tile animation properties.
+ * @param tile Which tile are we modifying
+ * @param length How many of the following tiles are attached to the animation
+ * @param slowness How slowly will the tile animate
+ */
+void CBMap::setTile(uint32_t tile, uint32_t length, uint32_t slowness) {
+	animLength[tile] = length;
 	animSlowness[tile] = slowness;
 }
 
-void CBMap::edit(uint8_t maplayer, int32_t MapX, int32_t MapY, int32_t tile){
-	int32_t position = MapY * mapWidth + MapX;
-	layers[maplayer][position] = tile;
+/** Manipulates the loaded map realtime.
+ * @param maplayer Which layer of the map are we modifying
+ * @param tileX,tileY Tile coordinates
+ * @param tileIndex The index of the tile to be drawn
+ */
+void CBMap::edit(uint8_t maplayer, int32_t tileX, int32_t tileY, int32_t tileIndex) {
+	int32_t position = tileY * mapWidth + tileX;
+	layers[maplayer][position] = tileIndex;
 }
 
-int32_t CBMap::getHit(int32_t x, int32_t y){
-	if(x < 0 || x >= mapWidth || y < 0 || y >= mapHeight)
+/** Returns the hit data in the given tile coordinates.
+ * @param x,y Tile coordinates
+ * @returns What data is there in the second layer of the map
+ */
+int32_t CBMap::getHit(int32_t x, int32_t y) {
+	if (x < 0 || x >= mapWidth || y < 0 || y >= mapHeight) {
+		// No hit outside the map
 		return 0;
+	}
+	// Calculate index position and return the data in hit layer (layer2)
 	int32_t position = y * mapWidth + x;
 	return layers[2][position];
 }
 
-int32_t CBMap::getMap(uint8_t maplayer, int32_t MapX, int32_t MapY){
-	if(MapX < 0 || MapX >= mapWidth || MapY < 0 || MapY >= mapHeight)
+/** Returns data from the given map layer.
+ * @param maplayer Which layer are we reading from
+ * @param tileX,tileY Tile coordinates
+ * @returns What data is there in the given layer
+ */
+int32_t CBMap::getMap(uint8_t maplayer, int32_t tileX, int32_t tileY) {
+	if (tileX < 0 || tileX >= mapWidth || tileY < 0 || tileY >= mapHeight) {
+		// There's no data outside the map region
 		return 0;
-	int32_t position = MapY * mapWidth + MapX;
+	}
+	// Calculate index position and return the data in the given layer
+	int32_t position = tileY * mapWidth + tileX;
 	return layers[maplayer][position];
 }
