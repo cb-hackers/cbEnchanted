ed31b3ed6cbaa738bf82f6e8c18bb27fc4be6875
Lassi Hämäläinen <Latexi95@gmail.com>
2012-05-27 18:53:36 +0300

    Added variablestack.cpp.

---
+++ src/variablestack.cpp
@@ -0,0 +1,11 @@
+#include "variablestack.h"
+#include "any.h"
+void VariableStack::resize(uint32_t newStackSize) {
+	Any *newStack = new Any[newStackSize];
+	memcpy(newStack, stackArray, sizeof(Any) * stackSize);
+	memset((newStack + stackSize), 0, (newStackSize - stackSize) * sizeof(Any));
+	delete [] stackArray;
+	stackArray = newStack;
+	stackSize = newStackSize;
+	INFO("Stack resized to %i", stackSize);
+}
