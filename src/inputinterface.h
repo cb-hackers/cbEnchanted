08da60cf2281af2ee528cf9db7308cb97d9daa84
Vesa Laakso <laakso.vesa@gmail.com>
2012-06-10 00:02:20 +0300

    Removed the obnoxious character arrays from InputInterface

--- src/inputinterface.h
+++ src/inputinterface.h
@@ -113,15 +113,6 @@ class InputInterface {
 		/** Last mouse positions for MouseMove* */
 		int32_t lastMouseX, lastMouseY, lastMouseZ;
 
-		/** Array of Allegro keys mapped to their uint8_t equivelant ASCII value */
-		uint8_t cbAsciiMap[ALLEGRO_KEY_MAX];
-
-		/** Array of Allegro keys mapped to their uint8_t equivelants ASCII value when Shift was pressed down. */
-		uint8_t cbAsciiMapShift[ALLEGRO_KEY_MAX];
-
-		/** Array of Allegro keys mapped to their uint8_t equivelants ASCII value when Alt+Ctrl or AltGr was pressed down. */
-		uint8_t cbAsciiMapAltGr[ALLEGRO_KEY_MAX];
-
 		/** Holds the current running Input */
 		CBInput *input;
 
